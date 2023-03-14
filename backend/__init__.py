f6c4311287cd4d829bdaefb7db7214b9988101c9
Aadesh-Baral <elkloppiko@gmail.com>
2023-03-14 11:46:16 +0545

    Add basic implementation of logic and endpoint to return related projects

--- backend/__init__.py
+++ backend/__init__.py
@@ -141,6 +141,7 @@ def add_api_endpoints(app):
         ProjectsQueriesAoiAPI,
         ProjectsQueriesPriorityAreasAPI,
         ProjectsQueriesFeaturedAPI,
+        ProjectQueriesRelatedProjectsAPI,
     )
     from backend.api.projects.activities import (
         ProjectsActivitiesAPI,
@@ -342,6 +343,10 @@ def add_api_endpoints(app):
     )
     api.add_resource(
         ProjectsQueriesFeaturedAPI, format_url("projects/queries/featured/")
+    )
+    api.add_resource(
+        ProjectQueriesRelatedProjectsAPI,
+        format_url("projects/queries/<int:project_id>/related-projects/"),
     )
 
     # Projects' addtional resources
